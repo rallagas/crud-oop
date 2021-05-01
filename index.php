e5c562477aff046de502433d89f141f4783efc3f
rallagas <rallagas@bicol-u.edu.ph>
2021-05-02 01:50:37 +0800

    Class DB.php

--- index.php
+++ index.php
@@ -3,3 +3,10 @@ require_once 'core/init.php';
 
 /*echo Config::get('mysql/username'); //127.0.0.1*/
 
+$user = DB::getInstance()->get('users', array('username','=','Reymar'));
+
+if( !$user->cnt() ){
+    echo 'Not exists';
+}else{
+    echo 'Ok';
+}
