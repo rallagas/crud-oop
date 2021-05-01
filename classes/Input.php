2d74e84a6608a551bf7b79e0c4650e6f71d90b2f
rallagas <rallagas@bicol-u.edu.ph>
2021-05-02 04:03:56 +0800

    Validation

--- classes/Input.php
+++ classes/Input.php
@@ -15,5 +15,15 @@ class Input {
                 
         }
     }
+
+    public static function get($item){
+        if(isset($_POST[$item])){
+            return $_POST[$item];
+        }
+        else if (isset($_GET[$item])){
+            return $_GET[$item];
+        }
+    return '';
+    }
     
 }
--- register.php
+++ register.php
@@ -13,22 +13,51 @@
                       <div class="mb-3">
                           <?php                          
                             if(Input::exists()){
-                                
+                                $validate = new Validate();
+                                $validation = $validate->check($_POST,array(
+                                    'username' => array(
+                                         'required' => true,
+                                         'min' => 2,
+                                         'max' => 20,
+                                         'unique' => 'users'
+                                    ),
+                                    'password' => array(
+                                        'required' => true,
+                                        'min' => 6
+                                    ),
+                                    'password_again' => array(
+                                        'required' => true,
+                                        'matches' => 'password'
+                                    ),
+                                    'name' => array(
+                                        'required' => true,
+                                        'min' => 2,
+                                        'max' => 50
+                                    )
+                                ));
+
+                                if($validation->passed()){
+                                    echo 'Passed'; //do something if everything is ok
+                                }else{
+                                    foreach($validation->errors() as $error){
+                                        echo "<div class='alert alert-danger'> <i class='bi bi-exclamation-triangle'></i> ".$error."</div>";
+                                    }
+                                }
                             }
                           ?>
                       </div>
                     <form action="" method="post">
                       <div class="mb-3">
-                          <input type="text" name="p_username" value="" autocomplete="off" placeholder="Username" class="form-control">
+                          <input type="text" name="username" value="<?php echo Input::get('username'); ?>" autocomplete="off" placeholder="Username" class="form-control">
                       </div>
                        <div class="mb-3">
-                           <input type="password" name="p_password" placeholder="Choose a Password" class="form-control">
+                           <input type="password" name="password" placeholder="Choose a Password" class="form-control">
                        </div>
                        <div class="mb-3">
-                           <input type="password" name="p_password_again" placeholder="Password Again" class="form-control">
+                           <input type="password" name="password_again" placeholder="Password Again" class="form-control">
                        </div>
                        <div class="mb-3">
-                           <input type="text" name="p_name" placeholder="Full Name Again" class="form-control">
+                           <input type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" placeholder="Full Name Again" class="form-control">
                        </div>
                        <button type="submit" class="btn btn-success form-btn"> <i class="bi bi-check"></i> Done </button>
                    </form>
