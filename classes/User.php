6e3626d213d3f4536c7a2d378e21855e380769b3
rallagas <rallagas@bicol-u.edu.ph>
2021-05-07 02:11:16 +0800

    Update User.php

--- classes/User.php
+++ classes/User.php
@@ -47,7 +47,7 @@ class User{
         }
     }
 
-    public function login($username = null, $password = null, $remember){
+    public function login($username = null, $password = null, $remember = false){
         $user = $this->find($username);
         
         if($user){
